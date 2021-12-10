2ef192912abcda8db8aff3b623666f2e289a97e3
JacobFYeomans <90638405+JacobFYeomans@users.noreply.github.com>
2021-12-10 10:39:17 -0400

    Initial commit

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerScript : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public GameObject Player;
+
+    public bool OneTimeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+            Player.GetComponent<S_FPS_Move>().enabled = false;
+
+            if (OneTimeTrigger == true)
+            {
+
+                Destroy(this.gameObject);
+
+            }
+
+        }
+    }
+
+
+
+
+}
